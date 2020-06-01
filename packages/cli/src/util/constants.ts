5f05aec129164f63ddcbf821d018ee3c880313ad
Zak Barbuto <zbarbuto@nextfaze.com>
2020-06-01 14:35:33 +0930

    Update default classification schemes

--- packages/cli/src/util/constants.ts
+++ packages/cli/src/util/constants.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_CLASSIFICATION_SCHEME = 'esafe';
-export const DEFAULT_CLASSIFICATION_NAME = 'esafe_templates';
+export const DEFAULT_CLASSIFICATION_SCHEME = 'story_category';
+export const DEFAULT_CLASSIFICATION_NAME = 'story_item';
 export const SLOT_TYPE_BLACKLIST = ['image', 'classification_node'];
 export const ITEM_ASSOCIATIONS = ['slots', 'thumbnails', 'attachments'];
--- packages/cli/test/commands/items/create-config.test.ts
+++ packages/cli/test/commands/items/create-config.test.ts
@@ -1,5 +1,9 @@
 import { expect } from '@oclif/test';
 import { readFileSync } from 'fs';
+import {
+  DEFAULT_CLASSIFICATION_NAME,
+  DEFAULT_CLASSIFICATION_SCHEME
+} from '../../../src/util/constants';
 import { customTest, outputFixture, testEnvironmentFile, testUserAuth } from '../../test-helpers';
 
 describe('items:create-config', () => {
@@ -56,8 +60,8 @@ function mockVault(api) {
   api
     .get('/item_templates')
     .query({
-      'by_classification[scheme]': 'esafe',
-      'by_classification[name]': 'esafe_templates'
+      'by_classification[scheme]': DEFAULT_CLASSIFICATION_SCHEME,
+      'by_classification[name]': DEFAULT_CLASSIFICATION_NAME
     })
     .matchHeader('Authorization', '2FPN4n5T68xy78i6HHuQ')
     .matchHeader('Meeco-Subscription-Key', 'environment_subscription_key')
