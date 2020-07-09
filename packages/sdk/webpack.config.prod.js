7bbb4b9259dc3a0287f088794376791b021f40b6
zbarbuto <zbarbuto@nextfaze.com>
2020-07-09 16:40:24 +0930

    Use webpack instead of parcel for the sdk demo (#45)

---
+++ packages/sdk/webpack.config.prod.js
@@ -0,0 +1,12 @@
+const config = require('./webpack.config');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  ...config,
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()]
+  },
+  plugins: [...(config.plugins || []), new CleanWebpackPlugin()]
+};
