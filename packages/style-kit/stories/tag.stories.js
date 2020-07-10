dc71a9565b5b33b0c99752db94be9aeebbcd5d78
Izzy <isabellepeskett@gmail.com>
2020-07-10 17:21:43 +1000

    Revised tag scoping

--- packages/style-kit/stories/tag.stories.js
+++ packages/style-kit/stories/tag.stories.js
@@ -5,24 +5,12 @@ import '../src/components/icons';
 export const tags = () =>
   /*html*/
   `<div class="${story}">
-    <p>Basic Tag</>
-    <div class="tag">
-      <span>Tag name</span>
-    </div>
-
-    <p>Edit Mode Tag</p>
-    <div class="tag">
+    <span class="tag">Tag name</span>
       <span class="tag-edit">
         Tag edit mode<meeco-icon icon="close"></meeco-icon>
       </span>
-    </div>
-
-    <p>Tag with Icon</p>
-    <div class="tag">
       <span class="tag-icon">
         <meeco-icon icon="contactNew"></meeco-icon>
         Tag with Icon
       </span>
-    </div>
-
   </div>`;
