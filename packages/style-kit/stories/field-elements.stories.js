845544bacde6b83817711779edd72b8763702bc4
Izzy <isabellepeskett@gmail.com>
2020-07-10 17:01:47 +1000

    Input with value text

--- packages/style-kit/stories/field-elements.stories.js
+++ packages/style-kit/stories/field-elements.stories.js
@@ -5,37 +5,11 @@ import '../src/components/icons';
 
 const meecoIcons = icon => `<meeco-icon icon=${icon}></meeco-icon>`;
 
-export const inputs = () => {
-  const container = document.createElement('div');
-  container.className = story;
-
-  const types = ['', 'Text Value', 'With Label'];
-
-  types.forEach(type => {
-    const inputField = document.createElement('input');
-    inputField.type = 'text';
-    inputField.placeholder = type;
-
-    if (inputField.placeholder === 'With Label') {
-      const inputLabel = document.createElement('div');
-      const label = document.createElement('label');
-      label.innerHTML = 'Label';
-      inputLabel.appendChild(label);
-      inputLabel.appendChild(inputField);
-      container.appendChild(inputLabel);
-    } else {
-      container.appendChild(inputField);
-    }
-  });
-
-  const searchBar = document.createElement('div');
-  searchBar.className = 'search-bar';
-  searchBar.innerHTML = meecoIcons('search');
-  const input = document.createElement('input');
-  input.type = 'search';
-  input.placeholder = 'Search';
-  searchBar.appendChild(input);
-  container.appendChild(searchBar);
-
-  return container;
-};
+export const editMode = () => /*html*/ `
+<div class=${story}>
+  <p>Field elements in edit mode</p>
+  <div class="container">
+    <input type="text" value="Input field with text"/>
+  </div>
+</div>
+`;
