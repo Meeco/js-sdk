b3313dbd9313396fa7e6a4602f570157b9255dbe
Izzy <isabellepeskett@gmail.com>
2020-07-14 14:47:32 +1000

    Initiated controls stories

---
+++ packages/style-kit/stories/controls.stories.js
@@ -0,0 +1,11 @@
+export default { title: 'Controls' };
+import { story } from 'style-loader!./controls.stories.scss';
+
+export const toggle = () => /*html*/ `
+<div class=${story}>
+  <p>Toggle</p>
+
+  <p>Checkbox</p>
+
+  </p>Radio button</p>
+</div>`;
