2dc91a126c910f0d6094d72f1d8a1895b7d32bc5
Izzy <isabellepeskett@gmail.com>
2020-07-14 14:01:43 +1000

    Added date field, dropdown & search bar

--- packages/style-kit/stories/field-elements.stories.js
+++ packages/style-kit/stories/field-elements.stories.js
@@ -3,41 +3,37 @@ export default { title: 'Field Elements' };
 import { story } from 'style-loader!./field-elements.stories.scss';
 import '../src/components/icons';
 import placeholder from '../assets/image-placeholder.png';
-
-// Getting today's date in correct format
-const today = new Date(Date.now());
-const dd = today.getDate();
-const mmm = today.toDateString().substring(4, 7);
-const yyyy = today.getFullYear();
-let hours = today.getHours();
-let minutes = today.getMinutes();
-const format = hours >= 12 ? 'pm' : 'am';
-hours = hours % 12;
-hours = hours ? hours : 12;
-minutes = minutes < 10 ? '0' + minutes : minutes;
-
-const dateValue = `${dd} ${mmm} ${yyyy}, ${hours}:${minutes}${format}`;
+import { useEffect } from '@storybook/client-api';
 
 export const editMode = () => /*html*/ `
 <div class=${story}>
   <p>Field elements in edit mode</p>
   <div class="container">
     <input type="text" value="Input field with text"/>
+
     <input type="text" placeholder="Placeholder text"/>
+
     <label for="label">Label</label>
     <input name="label" type="text" value="Field value"/>
+
     <select class="dropdown-input">
       <option>Option 1</option>
       <option>Option 2</option>
       <option>Option 3</option>
     </select>
+
+    <label>Date field label</label>
+    <input type="date" value="2020-07-13" />
+
+    <input type="search" placeholder="Search placeholder"/>
   </div>
+
 </div>
 `;
 
 export const viewMode = () => /*html*/ `
 <div class=${story}>
-  <h3>Field elements in View Mode</h3>
+  <p>Field elements in View Mode</p>
 
     <div class="container">
       <p class="label">Text Field Label</p>
@@ -53,13 +49,5 @@ export const viewMode = () => /*html*/ `
       </div>
     </div>
 
-    <div class="container">
-      <label>Date field label</label>
-      <input type="date" value="2020-07-13" disabled="true" />
-    </div>
-
-    <div class="container">
-      <input type="search" placeholder="Search placeholder"/>
-    </div>
 
 </div>`;
