7c153bdbfbebc27c75784546319217c53ddb29df
marselbougrini <mars@nonverbal.com.au>
2020-07-15 17:45:24 +0930

    Added initial version of meeco icon font

--- packages/style-kit/stories/icons.stories.js
+++ packages/style-kit/stories/icons.stories.js
@@ -1,5 +1,118 @@
 export default { title: 'Icons' };
 
-import '../src/components/icons';
+export const meecoIcons = () => `
 
-export const meecoIcons = () => `<meeco-icon icon="share"></meeco-icon>`;
+<style>
+  .meeco-icons-demo{
+    font-size:24px;
+  }
+</style>
+
+<h2>Meeco Icon Set</h2>
+
+<div class="meeco-icons-demo">
+
+  <span class="icon-add-circled-reverse"></span>
+  <span class="icon-add-circled"></span>
+  <span class="icon-add"></span>
+  <span class="icon-alert-circled-reverse"></span>
+  <span class="icon-alert-circled"></span>
+  <span class="icon-alert-octagon-reverse"></span>
+  <span class="icon-alert-octagon"></span>
+  <span class="icon-alert-triangle-reverse"></span>
+  <span class="icon-alert-triangle"></span>
+  <span class="icon-arrow-down"></span>
+  <span class="icon-arrow-left"></span>
+  <span class="icon-arrow-right"></span>
+  <span class="icon-arrow-up"></span>
+  <span class="icon-attachment"></span>
+  <span class="icon-badge"></span>
+  <span class="icon-calendar"></span>
+  <span class="icon-camera"></span>
+  <span class="icon-card-stack"></span>
+  <span class="icon-chevron-down-circled-reverse"></span>
+  <span class="icon-chevron-down-circled"></span>
+  <span class="icon-chevron-down"></span>
+  <span class="icon-chevron-left-circled-reverse"></span>
+  <span class="icon-chevron-left-circled"></span>
+  <span class="icon-chevron-left"></span>
+  <span class="icon-chevron-right-circled-reverse"></span>
+  <span class="icon-chevron-right-circled"></span>
+  <span class="icon-chevron-right"></span>
+  <span class="icon-chevron-up-circled-reverse"></span>
+  <span class="icon-chevron-up-circled"></span>
+  <span class="icon-chevron-up"></span>
+  <span class="icon-clipboard"></span>
+  <span class="icon-contact-new"></span>
+  <span class="icon-contact"></span>
+  <span class="icon-contacts"></span>
+  <span class="icon-credit-card"></span>
+  <span class="icon-cross-circled-reverse"></span>
+  <span class="icon-cross-circled"></span>
+  <span class="icon-cross"></span>
+  <span class="icon-delete"></span>
+  <span class="icon-download"></span>
+  <span class="icon-duplicate"></span>
+  <span class="icon-edit"></span>
+  <span class="icon-feed"></span>
+  <span class="icon-field"></span>
+  <span class="icon-file-text"></span>
+  <span class="icon-file"></span>
+  <span class="icon-filter"></span>
+  <span class="icon-hamburger-menu"></span>
+  <span class="icon-home"></span>
+  <span class="icon-id"></span>
+  <span class="icon-image"></span>
+  <span class="icon-info-circled-reverse"></span>
+  <span class="icon-info-circled"></span>
+  <span class="icon-integrations"></span>
+  <span class="icon-item"></span>
+  <span class="icon-kebab-circled-reverse"></span>
+  <span class="icon-kebab-circled"></span>
+  <span class="icon-kebab"></span>
+  <span class="icon-list"></span>
+  <span class="icon-login"></span>
+  <span class="icon-logout"></span>
+  <span class="icon-me-filled"></span>
+  <span class="icon-me-outlined"></span>
+  <span class="icon-me"></span>
+  <span class="icon-meatballs-circled-reverse"></span>
+  <span class="icon-meatballs-circled"></span>
+  <span class="icon-meatballs"></span>
+  <span class="icon-meeco-free"></span>
+  <span class="icon-meeco"></span>
+  <span class="icon-notes"></span>
+  <span class="icon-notification"></span>
+  <span class="icon-qr-code"></span>
+  <span class="icon-question-circled-reverse"></span>
+  <span class="icon-question-circled"></span>
+  <span class="icon-redo"></span>
+  <span class="icon-refresh"></span>
+  <span class="icon-reminder"></span>
+  <span class="icon-remove-circled-reverse"></span>
+  <span class="icon-remove-circled"></span>
+  <span class="icon-remove"></span>
+  <span class="icon-reorder"></span>
+  <span class="icon-scan"></span>
+  <span class="icon-search"></span>
+  <span class="icon-settings"></span>
+  <span class="icon-share"></span>
+  <span class="icon-shared"></span>
+  <span class="icon-sharing"></span>
+  <span class="icon-tag"></span>
+  <span class="icon-template"></span>
+  <span class="icon-tick-circled-reverse"></span>
+  <span class="icon-tick-circled"></span>
+  <span class="icon-tick"></span>
+  <span class="icon-undo"></span>
+  <span class="icon-vault"></span>
+  <span class="icon-vault2"></span>
+  <span class="icon-verified-outline"></span>
+  <span class="icon-verified-reverse"></span>
+  <span class="icon-wallet"></span>
+  <span class="icon-website"></span>
+  <span class="icon-zoom"></span>
+
+</div>
+  
+  `;
