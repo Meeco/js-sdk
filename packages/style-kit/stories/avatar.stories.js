8cc164d820e5b4942ca6e39232a986a7b8a30984
izzypesk <izzy.peskett@meeco.me>
2020-08-05 14:10:31 +1000

    Created avatar docs

--- packages/style-kit/stories/avatar.stories.js
+++ packages/style-kit/stories/avatar.stories.js
@@ -14,47 +14,24 @@ export default {
 export const avatar = () =>
   /*html*/
   `<div class=${story}>
-      <p>Avatar Standard</p>
-
-      <span class="avatar">ME</span>
-      <span class="avatar">JV</span>
-      <span class="avatar">KD</span>
-
-      <p> Avatar Large </p>
-
-      <span class="avatar large">ME</span>
-
-      <p> Avatar Small </p>
-
-      <span class="avatar small">ME</span>
-
-      <p>Avatar Stack</p><br/>
-      <p class="small"> Avatar stack can take the three avatar sizes - small, standard and large.</p>
-      <p class="small"> The default avatar stack size to use is small.</p>
-
-      <div class="avatar-stack">
-        <div class="avatar small">ME</div>
-        <div class="avatar small">KD</div>
-        <div class="avatar small">JV</div>
-        <div class="avatar small">+3</div>
-      </div>
-
-      <p class="small">An avatar stack can also take an image and initials, the image will take priority for display</p>
-
-      <div class="avatar-stack">
-        <div class="avatar small">ME</div>
-        <div class="avatar small">JV<img class="avatar-img" src=${avatarImg}/></div>
-        <div class="avatar small">KD</div>
-      </div>
-
-      <p class="small">For now the avatar stack has a default colour which can be set in the theme, to override add custom background color and font color</p>
-
-      <div class="avatar-stack">
-        <div class="avatar small" style="background-color:#F2FAEA; color:#7FC92B">ME</div>
-        <div class="avatar small">JV<img class="avatar-img" src=${avatarImg}/></div>
-        <div class="avatar small" style="background-color:#FBEAF3; color:#DB2C87;">KD</div>
-        <div class="avatar small">+3</div>
-      </div>
-
+      <p><span class="avatar small">ME</span>This is a small avatar</p>
+      <p><span class="avatar">ME</span>This is a default avatar</p>
+      <p><span class="avatar large">ME</span>This is a large avatar</p>
     </div>
   `;
+
+export const avatarStack = () => /*html*/ `
+  <div class=${story}>
+    <div class="avatar-stack">
+      <div class="avatar small">ME</div>
+      <div class="avatar small">JV<img class="avatar-img" src=${avatarImg}/></div>
+      <div class="avatar small">KD</div>
+    </div>
+
+    <div class="avatar-stack">
+      <div class="avatar small" style="background-color:#F2FAEA; color:#7FC92B">ME</div>
+      <div class="avatar small">JV<img class="avatar-img" src=${avatarImg}/></div>
+      <div class="avatar small" style="background-color:#FBEAF3; color:#DB2C87;">KD</div>
+      <div class="avatar small">+3</div>
+    </div>
+  </div>`;
