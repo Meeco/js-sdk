ef4ade4dd2a40a2c9f54c6697a146762c8ee0fdc
izzypesk <izzy.peskett@meeco.me>
2020-08-05 15:17:36 +1000

    Created card docs

--- packages/style-kit/stories/card.stories.js
+++ packages/style-kit/stories/card.stories.js
@@ -1,7 +1,16 @@
+import mdx from './card.mdx';
 import { story } from 'style-loader!./card.stories.scss';
 import { withKnobs, text } from '@storybook/addon-knobs';
 
-export default { title: 'Cards and Shadows', decorators: [withKnobs] };
+export default {
+  title: 'Cards',
+  decorators: [withKnobs],
+  parameters: {
+    docs: {
+      page: mdx
+    }
+  }
+};
 
 export const shadows = () => /*html*/ `
 <div class="${story} shadows">
@@ -14,86 +23,56 @@ export const shadows = () => /*html*/ `
 </div>
 `;
 
-export const cards = () => {
-  const basicTitle = text('Basic Card Title', 'Basic Card');
-  const basicSubtitle = text('Basic Card Label', 'Sub-label (Optional)');
-  const footerText = text('Card Footer Text', 'Some Footer Text');
-
-  return /*html*/ `
-
-<div class="${story} advanced">
-  <p>Basic Card</p>
-  <div class="card basic">
-    <div class="content">
-      <div class="icon"></div>
-      <div>
-        <p class="card-label">${basicTitle}</p>
-        ${basicSubtitle ? `<p class="subtitle">${basicSubtitle}</p>` : ''}
-      </div>
-    </div>
-  </div>
-
-  <p>Card with a Footer</p>
-  <div class="card">
-    <div class="content">
-      <div class="icon"></div>
-      <p class="card-label">Shared Card Label</p>
-    </div>
-    <div class="footer">
-      <p class="subtitle">${footerText}</p>
-    </div>
-  </div>
-
-  <p>Complex Footers</p>
-  <div class="card complex-footer">
-    <div class="content">
-      <div class="icon"></div>
-      <div>
-        <p class="card-label">Example Complex Footer</p>
-      </div>
-    </div>
-    <div class="footer">
-      <p class="subtitle">Label</p>
-      <div>
-        <p>More Text</p>
+export const basicCard = () => /*html*/ `
+  <div class="${story} advanced">
+    <p>Basic Card</p>
+    <div class="card basic">
+      <div class="content">
         <div class="icon"></div>
+        <div>
+          <p class="card-label">Basic Card Label</p>
+          <p class="subtitle">card subtitle</p>
+        </div>
       </div>
     </div>
-  </div>
+  </div>`;
 
-  <h4>Content Wrapping</h4>
-  <p>Unbounded</p>
-  ${largeCard()}
+export const cardWithFooter = () => /*html*/ `
+    <div class="${story} advanced">
+      <p>Card with a Footer</p>
+      <div class="card">
+        <div class="content">
+          <div class="icon"></div>
+          <p class="card-label">Default Card Label</p>
+        </div>
+        <div class="footer">
+          <p class="subtitle">Footer subtitle</p>
+        </div>
+      </div>
+    </div>`;
 
-  <p>Bounded</p>
-  <div class="fixed-width">
-    ${largeCard()}
-  </div>
-</div>
-`;
-};
-
-const largeCard = () => /*html*/ `
-<div class="card">
-  <div class="content">
-    <div class="icon"></div>
-    <div>
-      <p class="card-label">Card Title</p>
-      <p class="subtitle">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Aliquid velit atque odio iure dolore nihil praesentium
-        delectus animi doloribus eveniet, veritatis officia adipisci
-        officiis, dolorem suscipit obcaecati possimus porro nisi.
-      </p>
-    </div>
-  </div>
-</div>
-`;
+export const cardWithComplexFooter = () => /*html*/ `
+    <div class="${story} advanced">
+      <p>Complex Footers</p>
+      <div class="card complex-footer">
+        <div class="content">
+          <div class="icon"></div>
+          <div>
+            <p class="card-label">Example Complex Footer</p>
+          </div>
+        </div>
+        <div class="footer">
+          <p class="subtitle">Label</p>
+          <div>
+            <p>More Text</p>
+            <div class="icon"></div>
+          </div>
+        </div>
+      </div>
+    </div>`;
 
 export const tile = () => /*html*/ `
 <div class=${story}>
-  <p>Tile</p>
-  <p class="small">Tiles are similiar to cards, except they are either square or taller than they are wide</p>
   <div class="tile">
     <div class="content">
       <div class="icon"></div>
