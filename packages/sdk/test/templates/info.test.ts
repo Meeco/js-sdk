6da65165cea4d6854b87b9189956475d478a3a57
wbalce <wilkinson.balce@meeco.me>
2020-09-15 14:42:31 +1000

    Add template tests and create config test for items

---
+++ packages/sdk/test/templates/info.test.ts
@@ -0,0 +1,72 @@
+import { expect } from '@oclif/test';
+import { vaultAPIFactory } from '../../src/util/api-factory';
+import { customTest, environment, getOutputFixture, replaceUndefinedWithNull, testUserAuth } from '../test-helpers';
+
+describe('templates:info', () => {
+  customTest
+    .nock('https://sandbox.meeco.me/vault', api => {
+      api
+        .get('/item_templates')
+        .matchHeader('Authorization', '2FPN4n5T68xy78i6HHuQ')
+        .matchHeader('Meeco-Subscription-Key', 'environment_subscription_key')
+        .reply(200, response);
+    })
+    .it('fetches info about a particular template', async () => {
+      const service = vaultAPIFactory(environment)(testUserAuth).ItemTemplateApi;
+      const { slots: resultingSlots, item_templates: resultingTemplates } = await service.itemTemplatesGet();
+
+      const { slots: expectedSlots, ...expectedTemplate } = getOutputFixture('info-template.output.yaml').spec;
+      const requiredTemplate = resultingTemplates.find(template => template.name === 'drink')!;
+      const requiredSlots = resultingSlots.filter(slot => requiredTemplate.slot_ids.includes(slot.id));
+
+      expect(replaceUndefinedWithNull(requiredTemplate)).to.eql(expectedTemplate);
+      expect(replaceUndefinedWithNull(requiredSlots)).to.deep.members(expectedSlots);
+    });
+});
+
+const response = {
+  item_templates: [
+    {
+      created_at: new Date(0),
+      updated_at: new Date(0),
+      name: 'food',
+      slots_ids: ['steak', 'pizza', 'yoghurt'],
+    },
+    {
+      created_at: new Date(0),
+      updated_at: new Date(0),
+      name: 'drink',
+      slot_ids: ['yoghurt', 'water', 'beer'],
+    },
+  ],
+  slots: [
+    {
+      id: 'pizza',
+      created_at: new Date(0),
+      updated_at: new Date(0),
+    },
+    {
+      id: 'steak',
+      created_at: new Date(0),
+      updated_at: new Date(0),
+    },
+    {
+      id: 'yoghurt',
+      created_at: new Date(0),
+      updated_at: new Date(0),
+    },
+    {
+      id: 'water',
+      created_at: new Date(0),
+      updated_at: new Date(0),
+    },
+    {
+      id: 'beer',
+      created_at: new Date(0),
+      updated_at: new Date(0),
+    },
+  ],
+  attachments: [],
+  thumbnails: [],
+  classification_nodes: [],
+};
---
+++ packages/sdk/test/templates/list.test.ts
@@ -0,0 +1,70 @@
+import { expect } from '@oclif/test';
+import { vaultAPIFactory } from '../../src/util/api-factory';
+import { customTest, environment, getOutputFixture, testUserAuth } from '../test-helpers';
+
+const DEFAULT_CLASSIFICATION_NAME = 'esafe';
+const DEFAULT_CLASSIFICATION_SCHEME = 'esafe';
+
+describe('templates:list', () => {
+  customTest
+    .nock('https://sandbox.meeco.me/vault', api => {
+      api
+        .get('/item_templates')
+        .matchHeader('Authorization', '2FPN4n5T68xy78i6HHuQ')
+        .matchHeader('Meeco-Subscription-Key', 'environment_subscription_key')
+        .reply(200, response);
+    })
+    .it(
+      'fetches a list of available templates (no classification scheme or name provided)',
+      async () => {
+        const service = vaultAPIFactory(environment)(testUserAuth).ItemTemplateApi;
+        const result = await service.itemTemplatesGet();
+        const expected = getOutputFixture('list-templates.output.yaml');
+
+        const requiredTemplateNames = result.item_templates.map(x => x.name);
+        expect(requiredTemplateNames).to.members(expected.spec);
+      }
+    );
+
+  customTest
+    .nock('https://sandbox.meeco.me/vault', api => {
+      api
+        .get('/item_templates')
+        .query({
+          'by_classification[scheme]': DEFAULT_CLASSIFICATION_SCHEME,
+          'by_classification[name]': DEFAULT_CLASSIFICATION_NAME,
+        })
+        .matchHeader('Authorization', '2FPN4n5T68xy78i6HHuQ')
+        .matchHeader('Meeco-Subscription-Key', 'environment_subscription_key')
+        .reply(200, response);
+    })
+    .it('fetches a list of available templates scoped to classification scheme and name', async () => {
+      const service = vaultAPIFactory(environment)(testUserAuth).ItemTemplateApi;
+      const result = await service.itemTemplatesGet(DEFAULT_CLASSIFICATION_SCHEME, DEFAULT_CLASSIFICATION_NAME);
+      const requiredTemplateNames = result.item_templates.map(x => x.name);
+
+      const expected = getOutputFixture('list-templates.output.yaml');
+      expect(requiredTemplateNames).to.members(expected.spec);
+    });
+});
+
+const response = {
+  item_templates: [
+    {
+      name: 'food',
+      slots_ids: ['steak', 'pizza', 'yoghurt'],
+    },
+    {
+      name: 'drink',
+      slot_ids: ['yoghurt', 'water', 'beer'],
+    },
+    {
+      name: 'activities',
+      slot_ids: ['sport', 'recreational'],
+    },
+  ],
+  slots: [],
+  attachments: [],
+  thumbnails: [],
+  classification_nodes: [],
+};
