493465a1bc8566643288712486fa87a8c0d51821
wbalce <wilkinson.balce@meeco.me>
2020-09-15 14:47:38 +1000

    Stub sdk for client-task-queue cli test

--- packages/cli/test/commands/client-task-queue/list.test.ts
+++ packages/cli/test/commands/client-task-queue/list.test.ts
@@ -1,40 +1,12 @@
+import { ClientTaskQueueService } from '@meeco/sdk';
+import { ClientTaskQueueResponse } from '@meeco/vault-api-sdk';
 import { expect } from '@oclif/test';
 import { readFileSync } from 'fs';
 import { customTest, outputFixture, testEnvironmentFile, testUserAuth } from '../../test-helpers';
 
 describe('client-task-queue:list', () => {
   customTest
-    .nock('https://sandbox.meeco.me/vault', api =>
-      api
-        .get('/client_task_queue')
-        .query({ supress_changing_state: true, state: 'todo' })
-        .matchHeader('Authorization', '2FPN4n5T68xy78i6HHuQ')
-        .matchHeader('Meeco-Subscription-Key', 'environment_subscription_key')
-        .reply(200, {
-          client_tasks: [
-            {
-              id: 'a',
-              state: 'todo',
-              work_type: 'update_share',
-              target_id: 'share_item_id_a',
-              additional_options: {},
-              last_state_transition_at: new Date(0),
-              report: {},
-              created_at: new Date(0),
-            },
-            {
-              id: 'b',
-              state: 'todo',
-              work_type: 'update_share',
-              target_id: 'share_item_id_b',
-              additional_options: {},
-              last_state_transition_at: new Date(0),
-              report: {},
-              created_at: new Date(0),
-            },
-          ],
-        })
-    )
+    .stub(ClientTaskQueueService.prototype, 'list', list as any)
     .stdout()
     .run(['client-task-queue:list', ...testUserAuth, ...testEnvironmentFile, '-s', 'Todo'])
     .it('list task that client suppose to perform', ctx => {
@@ -42,3 +14,36 @@ describe('client-task-queue:list', () => {
       expect(ctx.stdout.trim()).to.equal(expected.trim());
     });
 });
+
+function list(
+  vaultAccessToken: string,
+  isChangingStateSuppressed: boolean,
+  clientTaskQueueState: any
+): Promise<ClientTaskQueueResponse> {
+  return Promise.resolve({
+    next_page_after: null,
+    meta: [],
+    client_tasks: [
+      {
+        id: 'a',
+        state: 'todo',
+        work_type: 'update_share',
+        target_id: 'share_item_id_a',
+        additional_options: {},
+        last_state_transition_at: new Date(0),
+        report: {},
+        created_at: new Date(0)
+      },
+      {
+        id: 'b',
+        state: 'todo',
+        work_type: 'update_share',
+        target_id: 'share_item_id_b',
+        additional_options: {},
+        last_state_transition_at: new Date(0),
+        report: {},
+        created_at: new Date(0)
+      }
+    ],
+  });
+}
