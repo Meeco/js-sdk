0cc755b4b0b95252a73ce224bf7e42644c6b9d2d
wbalce <wilkinson.balce@meeco.me>
2020-09-15 16:56:48 +1000

    Comment out tests for commented out attachment cli commands

--- packages/cli/test/commands/items/attach-file.test.ts
+++ packages/cli/test/commands/items/attach-file.test.ts
@@ -1,66 +1,66 @@
-import { ItemService } from '@meeco/sdk';
-import { expect } from '@oclif/test';
-import * as mime from 'mime-types';
-import * as fileUtils from '../../../src/util/file';
-import { customTest, inputFixture, testEnvironmentFile, testUserAuth } from '../../test-helpers';
+// import { ItemService } from '@meeco/sdk';
+// import { expect } from '@oclif/test';
+// import * as mime from 'mime-types';
+// import * as fileUtils from '../../../src/util/file';
+// import { customTest, inputFixture, testEnvironmentFile, testUserAuth } from '../../test-helpers';
 
-const singleBluePixel = `iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPj/HwADBwIAMCbHYQAAAABJRU5ErkJggg==`;
-const textFile = 'Hello, World!';
+// const singleBluePixel = `iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPj/HwADBwIAMCbHYQAAAABJRU5ErkJggg==`;
+// const textFile = 'Hello, World!';
 
-const mockFileRead: any = (path: string) => {
-  if (path === './my/secret/file.txt') {
-    return Promise.resolve(Buffer.from(textFile, 'binary'));
-  } else if (path === './my/secret/image.png') {
-    return Promise.resolve(Buffer.from(singleBluePixel, 'base64'));
-  }
-};
+// const mockFileRead: any = (path: string) => {
+//   if (path === './my/secret/file.txt') {
+//     return Promise.resolve(Buffer.from(textFile, 'binary'));
+//   } else if (path === './my/secret/image.png') {
+//     return Promise.resolve(Buffer.from(singleBluePixel, 'base64'));
+//   }
+// };
 
-const mockLookup: any = (path: string) => {
-  if (path === './my/secret/image.png') {
-    return 'image/png';
-  } else if (path === './my/secret/file.txt') {
-    return 'text/plain';
-  } else {
-    return 'text/plain';
-  }
-};
+// const mockLookup: any = (path: string) => {
+//   if (path === './my/secret/image.png') {
+//     return 'image/png';
+//   } else if (path === './my/secret/file.txt') {
+//     return 'text/plain';
+//   } else {
+//     return 'text/plain';
+//   }
+// };
 
-describe('items:attach-file', () => {
-  customTest
-    .stub(fileUtils, 'readFileAsBuffer', mockFileRead)
-    .stub(mime, 'lookup', mockLookup)
-    .stub(ItemService.prototype, 'attachFile', attachFile as any)
-    .stdout()
-    .stderr()
-    .run([
-      'items:attach-file',
-      '-c',
-      inputFixture('text-file-attachment.input.yaml'),
-      ...testUserAuth,
-      ...testEnvironmentFile,
-    ])
-    .it('encrypts and uploads a file to a slot', ctx => {
-      expect(ctx.stderr).to.contain('File was successfully attached');
-    });
+// describe('items:attach-file', () => {
+//   customTest
+//     .stub(fileUtils, 'readFileAsBuffer', mockFileRead)
+//     .stub(mime, 'lookup', mockLookup)
+//     .stub(ItemService.prototype, 'attachFile', attachFile as any)
+//     .stdout()
+//     .stderr()
+//     .run([
+//       'items:attach-file',
+//       '-c',
+//       inputFixture('text-file-attachment.input.yaml'),
+//       ...testUserAuth,
+//       ...testEnvironmentFile,
+//     ])
+//     .it('encrypts and uploads a file to a slot', ctx => {
+//       expect(ctx.stderr).to.contain('File was successfully attached');
+//     });
 
-  customTest
-    .stub(fileUtils, 'readFileAsBuffer', mockFileRead)
-    .stub(mime, 'lookup', mockLookup)
-    .stub(ItemService.prototype, 'attachFile', attachFile as any)
-    .stdout()
-    .stderr()
-    .run([
-      'items:attach-file',
-      '-c',
-      inputFixture('image-file-attachment.input.yaml'),
-      ...testUserAuth,
-      ...testEnvironmentFile,
-    ])
-    .it('generates and uploads a thumbnail for the binary if it is an image', ctx => {
-      expect(ctx.stderr).to.contain('File was successfully attached');
-    });
-});
+//   customTest
+//     .stub(fileUtils, 'readFileAsBuffer', mockFileRead)
+//     .stub(mime, 'lookup', mockLookup)
+//     .stub(ItemService.prototype, 'attachFile', attachFile as any)
+//     .stdout()
+//     .stderr()
+//     .run([
+//       'items:attach-file',
+//       '-c',
+//       inputFixture('image-file-attachment.input.yaml'),
+//       ...testUserAuth,
+//       ...testEnvironmentFile,
+//     ])
+//     .it('generates and uploads a thumbnail for the binary if it is an image', ctx => {
+//       expect(ctx.stderr).to.contain('File was successfully attached');
+//     });
+// });
 
-function attachFile() {
-  return Promise.resolve();
-}
+// function attachFile() {
+//   return Promise.resolve();
+// }
--- packages/cli/test/commands/items/get-attachment.test.ts
+++ packages/cli/test/commands/items/get-attachment.test.ts
@@ -1,89 +1,89 @@
-import { ItemService } from '@meeco/sdk';
-import { expect } from '@oclif/test';
-import * as fileUtils from '../../../src/util/file';
-import { customTest, testEnvironmentFile, testUserAuth } from '../../test-helpers';
+// import { ItemService } from '@meeco/sdk';
+// import { expect } from '@oclif/test';
+// import * as fileUtils from '../../../src/util/file';
+// import { customTest, testEnvironmentFile, testUserAuth } from '../../test-helpers';
 
-const singleBluePixel = Buffer.from(
-  'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPj/HwADBwIAMCbHYQAAAABJRU5ErkJggg==',
-  'base64'
-);
+// const singleBluePixel = Buffer.from(
+//   'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPj/HwADBwIAMCbHYQAAAABJRU5ErkJggg==',
+//   'base64'
+// );
 
-describe('items:get-attachment', () => {
-  let written;
-  beforeEach(() => {
-    written = false;
-  });
+// describe('items:get-attachment', () => {
+//   let written;
+//   beforeEach(() => {
+//     written = false;
+//   });
 
-  customTest
-    .stub(fileUtils, 'writeFileContents', <any>((path, contents, options) => {
-      expect(path).to.eql('out_file.txt');
-      expect(contents).to.eql(singleBluePixel);
-      expect(options).to.eql({
-        flag: 'wx',
-      });
-      written = true;
-      return Promise.resolve();
-    }))
-    .stub(ItemService.prototype, 'downloadAttachment', downloadAttachment as any)
-    .stdout()
-    .stderr()
-    .run([
-      'items:get-attachment',
-      'my_attachment_id',
-      '-o',
-      'out_file.txt',
-      ...testUserAuth,
-      ...testEnvironmentFile,
-    ])
-    .it('downloads, decrypts and saves an attachment to a file', () => {
-      expect(written).to.eql(true);
-    });
+//   customTest
+//     .stub(fileUtils, 'writeFileContents', <any>((path, contents, options) => {
+//       expect(path).to.eql('out_file.txt');
+//       expect(contents).to.eql(singleBluePixel);
+//       expect(options).to.eql({
+//         flag: 'wx',
+//       });
+//       written = true;
+//       return Promise.resolve();
+//     }))
+//     .stub(ItemService.prototype, 'downloadAttachment', downloadAttachment as any)
+//     .stdout()
+//     .stderr()
+//     .run([
+//       'items:get-attachment',
+//       'my_attachment_id',
+//       '-o',
+//       'out_file.txt',
+//       ...testUserAuth,
+//       ...testEnvironmentFile,
+//     ])
+//     .it('downloads, decrypts and saves an attachment to a file', () => {
+//       expect(written).to.eql(true);
+//     });
 
-  customTest
-    .stub(fileUtils, 'writeFileContents', <any>((path, contents, options) => {
-      return Promise.reject({
-        code: 'EEXIST',
-      });
-    }))
-    .stub(ItemService.prototype, 'downloadAttachment', downloadAttachment as any)
-    .stdout()
-    .stderr()
-    .run([
-      'items:get-attachment',
-      'my_attachment_id',
-      '-o',
-      'out_file.txt',
-      ...testUserAuth,
-      ...testEnvironmentFile,
-    ])
-    .catch(err =>
-      expect(err.message)
-        .to.contain('The destination file')
-        .and.to.contain('exists')
-    )
-    .it('does not overwrite existing files');
+//   customTest
+//     .stub(fileUtils, 'writeFileContents', <any>((path, contents, options) => {
+//       return Promise.reject({
+//         code: 'EEXIST',
+//       });
+//     }))
+//     .stub(ItemService.prototype, 'downloadAttachment', downloadAttachment as any)
+//     .stdout()
+//     .stderr()
+//     .run([
+//       'items:get-attachment',
+//       'my_attachment_id',
+//       '-o',
+//       'out_file.txt',
+//       ...testUserAuth,
+//       ...testEnvironmentFile,
+//     ])
+//     .catch(err =>
+//       expect(err.message)
+//         .to.contain('The destination file')
+//         .and.to.contain('exists')
+//     )
+//     .it('does not overwrite existing files');
 
-  customTest
-    .stub(fileUtils, 'writeFileContents', <any>((path, contents, options) => {
-      return Promise.reject({
-        code: 'OTHER',
-      });
-    }))
-    .stub(ItemService.prototype, 'downloadAttachment', downloadAttachment as any)
-    .stdout()
-    .stderr()
-    .run([
-      'items:get-attachment',
-      'my_attachment_id',
-      '-o',
-      'out_file.txt',
-      ...testUserAuth,
-      ...testEnvironmentFile,
-    ])
-    .catch(err => expect(err.message).to.contain('Failed to write to destination file'))
-    .it('handles other file write errors');
-});
+//   customTest
+//     .stub(fileUtils, 'writeFileContents', <any>((path, contents, options) => {
+//       return Promise.reject({
+//         code: 'OTHER',
+//       });
+//     }))
+//     .stub(ItemService.prototype, 'downloadAttachment', downloadAttachment as any)
+//     .stdout()
+//     .stderr()
+//     .run([
+//       'items:get-attachment',
+//       'my_attachment_id',
+//       '-o',
+//       'out_file.txt',
+//       ...testUserAuth,
+//       ...testEnvironmentFile,
+//     ])
+//     .catch(err => expect(err.message).to.contain('Failed to write to destination file'))
+//     .it('handles other file write errors');
+// });
 
-function downloadAttachment(attachmentId, vaultAccessToken, dataEncryptionKey) {
-  return Promise.resolve(singleBluePixel);
-}
+// function downloadAttachment(attachmentId, vaultAccessToken, dataEncryptionKey) {
+//   return Promise.resolve(singleBluePixel);
+// }
