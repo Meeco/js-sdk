4a197c2f11a323ccfdb14c6fae8182a032cb7978
wbalce <wilkinson.balce@meeco.me>
2020-09-15 14:50:30 +1000

    Stub sdk for user cli tests

--- packages/cli/test/commands/users/get.test.ts
+++ packages/cli/test/commands/users/get.test.ts
@@ -1,16 +1,11 @@
-import { Session, SrpChallenge } from '@meeco/keystore-api-sdk';
 import { UserService } from '@meeco/sdk';
 import { expect } from '@oclif/test';
 import { readFileSync } from 'fs';
-import * as Nock from 'nock';
 import { customTest, inputFixture, outputFixture, testEnvironmentFile } from '../../test-helpers';
 
 describe('meeco users:get', () => {
   customTest
-    .nock('https://sandbox.meeco.me/keystore', stubKeystore)
-    .nock('https://sandbox.meeco.me/vault', stubVault)
-    .mockCryppo()
-    .mockSRP()
+    .stub(UserService.prototype, 'get', get as any)
     .stderr()
     .stdout()
     .run(['users:get', '-c', inputFixture('get-user.input.yaml'), ...testEnvironmentFile])
@@ -20,10 +15,7 @@ describe('meeco users:get', () => {
     });
 
   customTest
-    .nock('https://sandbox.meeco.me/keystore', stubKeystore)
-    .nock('https://sandbox.meeco.me/vault', stubVault)
-    .mockCryppo()
-    .mockSRP()
+    .stub(UserService.prototype, 'get', get as any)
     .stderr()
     .stdout()
     .run([
@@ -40,83 +32,13 @@ describe('meeco users:get', () => {
     });
 });
 
-function stubKeystore(api: Nock.Scope) {
-  api
-    .post('/srp/challenges', {
-      srp_a: '000000000CLIENTPUBLIC',
-      username: 'user-1',
-    })
-    .reply(200, {
-      challenge: <SrpChallenge>{
-        challenge_b: '00SERVERPUBLIC',
-        challenge_salt: '00SALT',
-      },
-    });
-  api
-    .post('/srp/session', {
-      srp_m: '00SALT:00SERVERPUBLIC:PROOF',
-      srp_a: '000000000CLIENTPUBLIC',
-      username: 'user-1',
-    })
-    .reply(200, {
-      session: <Session>{
-        session_authentication_string: 'keystore_auth_token',
-      },
-    });
-  api
-    .get('/key_encryption_key')
-    .matchHeader('Authorization', 'keystore_auth_token')
-    .matchHeader('Meeco-Subscription-Key', 'environment_subscription_key')
-    .reply(200, {
-      key_encryption_key: {
-        serialized_key_encryption_key: `key_encryption_key`,
-      },
-    });
-  api
-    .get('/data_encryption_keys/data_encryption_key_id')
-    .matchHeader('Authorization', 'keystore_auth_token')
-    .matchHeader('Meeco-Subscription-Key', 'environment_subscription_key')
-    .reply(200, {
-      data_encryption_key: {
-        serialized_data_encryption_key: `data_encryption_key`,
-      },
-    });
-  api
-    .get('/keypairs/external_id/auth')
-    .matchHeader('Authorization', 'keystore_auth_token')
-    .matchHeader('Meeco-Subscription-Key', 'environment_subscription_key')
-    .reply(200, {
-      keypair: {
-        public_key: '--PUBLIC_KEY--ABCD',
-        encrypted_serialized_key: '--PRIVATE_KEY--12324',
-        external_identifiers: [UserService.VAULT_PAIR_EXTERNAL_IDENTIFIER],
-      },
-    });
-}
-
-function stubVault(api: Nock.Scope) {
-  api
-    .get('/me')
-    .matchHeader(
-      'Authorization',
-      [
-        '[decrypted]vault_auth_token--PRIVATE_KEY--12324[decrypted with key_encryption_key[decrypted with derived_key_123.asupersecretpassphrase]]',
-      ].join('')
-    )
-    .reply(200, {
-      user: {
-        id: 'vault_user',
-        private_dek_external_id: 'data_encryption_key_id',
-      },
-      associations: [],
-    });
-  api
-    .post('/session', {
-      public_key: '--PUBLIC_KEY--ABCD',
-    })
-    .reply(200, {
-      session: {
-        encrypted_session_authentication_string: 'vault_auth_token',
-      },
-    });
+function get(password, secret) {
+  return Promise.resolve({
+    data_encryption_key: 'ZGF0YV9lbmNyeXB0aW9uX2tleVtkZWNyeXB0ZWQgd2l0aCBrZXlfZW5jcnlwdGlvbl9rZXlbZGVjcnlwdGVkIHdpdGggZGVyaXZlZF9rZXlfMTIzLmFzdXBlcnNlY3JldHBhc3NwaHJhc2VdXQ==',
+    key_encryption_key: 'a2V5X2VuY3J5cHRpb25fa2V5W2RlY3J5cHRlZCB3aXRoIGRlcml2ZWRfa2V5XzEyMy5hc3VwZXJzZWNyZXRwYXNzcGhyYXNlXQ==',
+    keystore_access_token: 'keystore_auth_token',
+    passphrase_derived_key: 'ZGVyaXZlZF9rZXlfMTIzLmFzdXBlcnNlY3JldHBhc3NwaHJhc2U=',
+    secret: '1.user-1.my_secret_key',
+    vault_access_token: '[decrypted]vault_auth_token--PRIVATE_KEY--12324[decrypted with key_encryption_key[decrypted with derived_key_123.asupersecretpassphrase]]'
+  });
 }
