dfe59a454c4cad1a9d47cf0f29d35e18ba131bbe
Josh Bax <josh.bax@meeco.me>
2020-09-17 17:20:24 +1200

    Fix typo in -a flag description

--- packages/cli/src/flags/auth-flags.ts
+++ packages/cli/src/flags/auth-flags.ts
@@ -4,7 +4,7 @@ export const authFlags = {
   auth: flags.string({
     char: 'a',
     required: true,
-    description: 'Authorization config file yaml file (if not using the default .user.yaml)',
+    description: 'Authorization config yaml file (if not using the default .user.yaml)',
     default: '.user.yaml',
   }),
 };
