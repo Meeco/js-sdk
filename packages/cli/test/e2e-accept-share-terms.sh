9fec062b39effc58fae361ba7f766eed9793f01d
Josh Bax <josh.bax@meeco.me>
2020-10-01 14:20:16 +1300

    Update all e2e tests and readme

--- packages/cli/test/e2e-accept-share-terms.sh
+++ packages/cli/test/e2e-accept-share-terms.sh
@@ -11,7 +11,7 @@ run items:create-config vehicle -a .Alice.yaml > .template_vehicle.yaml
 
 cat .template_vehicle.yaml |
 yq -y '(.spec.label) = "My Vehicle"'|
-yq -y '(.spec.slots[0].value) = "20000101"' | 
+yq -y '(.spec.slots[0].value) = "20000101"' |
 yq -y '(.spec.slots[1].value) = "ABC123"' |
 yq -y '(.spec.slots[2].value) = "Mazda"' |
 yq -y '(.spec.slots[3].value) = "Familia"' |
@@ -36,7 +36,7 @@ echo "item id: ${itemId}"
 echo "connection id: ${connectionId}"
 
 echo "Share"
-run shares:create-config --from .Alice.yaml --connectionId $connectionId -i $itemId > .share_Alice_Bob.yaml
+run shares:create-config --from .Alice.yaml --connection .connection_Alice_Bob.created.yaml -i .item_alice.yaml > .share_Alice_Bob.yaml
 
 echo "Share the card to 'Bob'"
 run shares:create -c .share_Alice_Bob.yaml -t acceptance_required > .share_Alice_Bob.created.yaml
