3790ba2a2cacabd722fdae147d09b51eaac555f0
Josh Bax <josh.bax@meeco.me>
2021-03-01 22:17:48 +1300

    FS: fix implementation of AzureBlockDownload to pass OIDC token
    Cancel now works regarless of promise result and other args

--- packages/file-storage-common/src/azure-block-download.ts
+++ packages/file-storage-common/src/azure-block-download.ts
@@ -10,64 +10,76 @@ import { BlobStorage } from './services/Azure';
 
 export class AzureBlockDownload {
   /**
-   * @param {String} url Where to download the file
-   *
+   * @param oidcToken Mandatory OpenId Connect access token.
+   * @param strategy Cryppo strategy, usually found in EncryptionArtifacts file.
+   * @param encryptionArtifact Settings usually found in EncryptionArtifacts file.
+   * @param range Optional range in bytes e.g 0-255.
+   * @param userCancelable A Promise that cancels the download if resolved.
    */
-  url: any;
-  constructor(url: string) {
+  static async downloadAndDecrypt(
+    url: string,
+    oidcToken: string,
+    dataEncryptionKey: EncryptionKey,
+    strategy: CipherStrategy,
+    encryptionArtifact: {
+      iv: string;
+      at: string;
+      ad: string;
+    },
+    range?: string,
+    userCancelable?: Promise<any>
+  ): Promise<Uint8Array> {
+    const data = await this.download(url, oidcToken, range, userCancelable);
+
+    const str = decryptWithKeyUsingArtefacts(
+      dataEncryptionKey,
+      bytesToBinaryString(data),
+      strategy,
+      encryptionArtifact
+    );
+
+    return new Uint8Array(str || binaryStringToBytes(''));
+  }
+
+  /**
+   * @param oidcToken Mandatory OpenId Connect access token.
+   * @param range Optional range in bytes e.g 0-255
+   * @param userCancelable A Promise that cancels the download if resolved.
+   */
+  static async download(
+    url: string,
+    oidcToken: string,
+    range?: string,
+    userCancelable?: Promise<any>
+  ): Promise<Uint8Array> {
     if (typeof url !== 'string') {
       throw new Error('url must be a string');
     }
 
-    this.url = url;
-  }
+    const header = {
+      ['authorizationoidc2']: `Bearer ${oidcToken}`,
+    };
 
-  /**
-   * Start downloading
-   */
-  async start(
-    dataEncryptionKey: EncryptionKey | null,
-    strategy: CipherStrategy | null,
-    encryptionArtifact: any,
-    range: string | null,
-    onCancel?: any
-  ) {
-    if (range) {
-      let block: any;
-      if (onCancel) {
-        const source = axios.CancelToken.source();
-        const cancelToken = source.token;
-        block = await Promise.race([BlobStorage.getBlock(this.url, range, cancelToken), onCancel]);
-        if (block === 'cancel') {
-          source.cancel('cancel');
-          return new Promise((_resolve, reject) => {
-            reject('cancel');
-          });
-        }
-      } else {
-        block = await BlobStorage.getBlock(this.url, range);
+    let block: any;
+
+    if (userCancelable) {
+      const source = axios.CancelToken.source();
+      const cancelToken = source.token;
+      let userCanceled = false;
+      block = await Promise.race([
+        BlobStorage.getBlock(url, range, header, cancelToken),
+        userCancelable.then(() => (userCanceled = true)),
+      ]);
+      if (userCanceled) {
+        source.cancel('cancel');
+        return new Promise((_resolve, reject) => {
+          reject('cancel');
+        });
       }
-
-      const data = new Uint8Array(block.data);
-      let byteNumbers: Uint8Array;
-      if (dataEncryptionKey && strategy && encryptionArtifact) {
-        const str = decryptWithKeyUsingArtefacts(
-          dataEncryptionKey,
-          bytesToBinaryString(data),
-          strategy,
-          encryptionArtifact
-        );
-
-        byteNumbers = new Uint8Array(str || binaryStringToBytes(''));
-      }
-      return new Promise(resolve => {
-        resolve(byteNumbers || data);
-      });
     } else {
-      const blob = await BlobStorage.getBlock(this.url, null);
-      return new Promise(resolve => {
-        resolve(blob.data);
-      });
+      block = await BlobStorage.getBlock(url, range, header);
     }
+
+    return new Uint8Array(block.data);
   }
 }
