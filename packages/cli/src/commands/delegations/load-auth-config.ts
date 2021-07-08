5ca7e727a57f0afa9396fc04a2359fff332014b9
spazworm <spazworm@gmail.com>
2021-07-08 13:31:09 +0930

    added delegations:load-auth-config method and added loaded_delegations to the authconfig model

---
+++ packages/cli/src/commands/delegations/load-auth-config.ts
@@ -0,0 +1,91 @@
+import { DelegationService, keystoreAPIFactory, UserService, vaultAPIFactory } from '@meeco/sdk';
+import { Connection } from '@meeco/vault-api-sdk';
+import { AuthConfig } from '../../configs/auth-config';
+import authFlags from '../../flags/auth-flags';
+import MeecoCommand from '../../util/meeco-command';
+
+export default class DelegationLoadAuthConfig extends MeecoCommand {
+  static description =
+    'Create a delegation inivitation for another user to become a delegate connection';
+
+  static flags = {
+    ...MeecoCommand.flags,
+    ...authFlags,
+  };
+
+  async run() {
+    const { flags } = this.parse(this.constructor as typeof DelegationLoadAuthConfig);
+    const { auth } = flags;
+    const environment = await this.readEnvironmentFile();
+    const authConfig = (await this.readConfigFromFile(AuthConfig, auth))?.overrideWithFlags(flags);
+    if (!authConfig) {
+      this.error('authConfig must be present');
+    }
+    if (!authConfig.delegation_id) {
+      this.error('--delegationId argument must be present');
+    }
+
+    try {
+      const connection = await this.getConnectionForDelegationId(environment, authConfig);
+      const delegationsService = new DelegationService(environment, this.updateStatus);
+      const delegationToken = (connection?.the_other_user.integration_data || {})[
+        'delegation_token'
+      ];
+      const keystoreApi = keystoreAPIFactory(environment)({
+        ...authConfig,
+        delegation_id: undefined,
+      });
+      const { delegation } = await keystoreApi.DelegationApi.delegationsDelegationTokenGet(
+        delegationToken
+      );
+      const kek = await delegationsService.getAccountOwnerKek(authConfig, delegation);
+      const vaultApi = vaultAPIFactory(environment)(authConfig);
+      const user = await vaultApi.UserApi.meGet();
+      const userService = new UserService(environment, this.updateStatus);
+      const dek = await userService.getDataEncryptionKey(
+        { ...authConfig, key_encryption_key: kek },
+        user.user.private_dek_external_id || ''
+      );
+
+      const newAuthConfig = new AuthConfig({
+        ...authConfig,
+        loaded_delegations: {
+          ...authConfig.loaded_delegations,
+          [connection.own.user_id]: {
+            data_encryption_key: dek,
+            key_encryption_key: kek,
+            connection_id: connection.own.id,
+            delegation_token: delegationToken,
+          },
+        },
+      });
+
+      this.printYaml(AuthConfig.encodeFromAuthConfig(newAuthConfig));
+    } catch (e) {
+      this.error('unexpected error: ' + JSON.stringify(e));
+    }
+  }
+
+  private async getConnectionForDelegationId(environment, authConfig: AuthConfig) {
+    const vaultApi = vaultAPIFactory(environment)({
+      ...authConfig,
+      delegation_id: undefined,
+    });
+    let cursor;
+    let connection: Connection | undefined;
+    do {
+      const { next_page_after, connections } = await vaultApi.ConnectionApi.connectionsGet(cursor);
+      cursor = next_page_after;
+      connection = connections.find(
+        conn => conn.the_other_user.user_id == authConfig.delegation_id
+      );
+      // finish if there is a cursor and a connection
+      // finish if there is is not a cursor
+      // continue if there is a cursor but not a connection
+    } while (cursor && !connection);
+    if (!connection) {
+      throw new Error('cannot find connection for delegation id');
+    }
+    return connection;
+  }
+}
--- packages/cli/src/configs/auth-config.ts
+++ packages/cli/src/configs/auth-config.ts
@@ -1,12 +1,4 @@
-import {
-  AuthData,
-  DelegationService,
-  keystoreAPIFactory,
-  SymmetricKey,
-  UserService,
-  vaultAPIFactory,
-  VaultAPIFactoryInstance,
-} from '@meeco/sdk';
+import { AuthData, SymmetricKey } from '@meeco/sdk';
 import { CLIError } from '@oclif/errors';
 import { IYamlConfig } from './yaml-config';
 
@@ -21,6 +13,24 @@ interface IAuthMetadata {
   oidc_token?: string;
 }
 
+interface IAuthSpec {
+  loaded_delegations?: { [user_id: string]: LoadedDelegationSpec };
+}
+
+interface LoadedDelegation {
+  data_encryption_key: SymmetricKey; // base64
+  key_encryption_key: SymmetricKey; // base64
+  connection_id: string;
+  delegation_token: string;
+}
+
+interface LoadedDelegationSpec {
+  data_encryption_key: string | SymmetricKey; // base64
+  key_encryption_key: string | SymmetricKey; // base64
+  connection_id: string;
+  delegation_token: string;
+}
+
 export class AuthConfig {
   static kind = 'Authentication';
 
@@ -32,6 +42,7 @@ export class AuthConfig {
   public readonly passphrase_derived_key: SymmetricKey;
   public delegation_id?: string;
   public oidc_token?: string;
+  public loaded_delegations?: { [user_id: string]: LoadedDelegation };
 
   constructor(data: {
     secret: string;
@@ -42,6 +53,7 @@ export class AuthConfig {
     passphrase_derived_key: SymmetricKey;
     delegation_id?: string;
     oidc_token?: string;
+    loaded_delegations?: { [user_id: string]: LoadedDelegation };
   }) {
     this.secret = data.secret;
     this.keystore_access_token = data.keystore_access_token;
@@ -51,13 +63,29 @@ export class AuthConfig {
     this.passphrase_derived_key = data.passphrase_derived_key;
     this.delegation_id = data.delegation_id;
     this.oidc_token = data.oidc_token;
+    this.loaded_delegations = data.loaded_delegations;
   }
 
-  static fromYamlConfig(yamlConfigObj: IYamlConfig<IAuthMetadata>): AuthConfig {
+  static fromYamlConfig(yamlConfigObj: IYamlConfig<IAuthMetadata, IAuthSpec>): AuthConfig {
     if (yamlConfigObj.kind !== AuthConfig.kind) {
       throw new CLIError(
         `Config file of incorrect kind: ${yamlConfigObj.kind} (expected '${AuthConfig.kind}')`
       );
+    }
+    const loadedDelegations = yamlConfigObj.spec?.loaded_delegations;
+    const loadedDelegationsDeserializedKeys = {};
+    if (loadedDelegations) {
+      Object.keys(loadedDelegations).map(key => {
+        loadedDelegationsDeserializedKeys[key] = {
+          ...loadedDelegations[key],
+          key_encryption_key: SymmetricKey.fromSerialized(
+            loadedDelegations[key].key_encryption_key as string
+          ),
+          data_encryption_key: SymmetricKey.fromSerialized(
+            loadedDelegations[key].data_encryption_key as string
+          ),
+        };
+      });
     }
     return new AuthConfig({
       ...yamlConfigObj.metadata!,
@@ -70,55 +98,13 @@ export class AuthConfig {
       passphrase_derived_key: SymmetricKey.fromSerialized(
         yamlConfigObj.metadata!.passphrase_derived_key as string
       ),
+      loaded_delegations: loadedDelegationsDeserializedKeys,
     });
   }
 
   overrideWithFlags(flags) {
     this.delegation_id = flags.delegationId ? flags.delegationId : undefined;
     return this;
-  }
-
-  async loadDelegationDekIfNeeded(environment, updateStatus): Promise<AuthConfig> {
-    if (this.delegation_id) {
-      const vaultApi = vaultAPIFactory(environment)(this);
-      const connection = await this.getConnectionForDelegationId(vaultApi);
-      const delegationsService = new DelegationService(environment, updateStatus);
-      const delegationToken = (connection?.own.integration_data || {})['delegation_token'];
-      const keystoreApiUser = keystoreAPIFactory(environment)({
-        ...this,
-        delegation_id: undefined,
-      });
-      const { delegation } = await keystoreApiUser.DelegationApi.delegationsDelegationTokenGet(
-        delegationToken
-      );
-      const kek = await delegationsService.getAccountOwnerKek(this, delegation);
-      const user = await vaultApi.UserApi.meGet();
-      this.key_encryption_key = kek;
-      const userService = new UserService(environment, updateStatus);
-      const dek = await userService.getDataEncryptionKey(
-        this,
-        user.user.private_dek_external_id || ''
-      );
-      this.data_encryption_key = dek;
-    }
-    return this;
-  }
-
-  private async getConnectionForDelegationId(vaultApi: VaultAPIFactoryInstance) {
-    let cursor;
-    let connection;
-    do {
-      const { next_page_after, connections } = await vaultApi.ConnectionApi.connectionsGet(cursor);
-      cursor = next_page_after;
-      connection = connections.find(conn => conn.own.user_id == this.delegation_id);
-      // finish if there is a cursor and a connection
-      // finish if there is is not a cursor
-      // continue if there is a cursor but not a connection
-    } while (cursor && !connection);
-    if (!connection) {
-      throw new Error('cannot find connection for delegation id');
-    }
-    return connection;
   }
 
   static fromMetadata(metadata: IAuthMetadata) {
@@ -129,7 +115,7 @@ export class AuthConfig {
     });
   }
 
-  static encodeFromAuthData(payload: AuthData): IYamlConfig<IAuthMetadata> {
+  static encodeFromAuthData(payload: AuthData): IYamlConfig<IAuthMetadata, IAuthSpec> {
     const payloadSortedAlphabetically: AuthData = Object.keys(payload)
       .sort()
       .reduce((prev, key) => ({ ...prev, [key]: payload[key] }), {} as any);
@@ -140,11 +126,49 @@ export class AuthConfig {
     };
   }
 
+  static encodeFromAuthConfig(payload: AuthConfig): IYamlConfig<IAuthMetadata, IAuthSpec> {
+    const payloadSortedAlphabetically: AuthConfig = Object.keys(payload)
+      .sort()
+      .reduce((prev, key) => ({ ...prev, [key]: payload[key] }), {} as any);
+    const loadedDelegations = payload.loaded_delegations;
+    const loadedDelegationsSerializedKeys = {};
+    if (loadedDelegations) {
+      Object.keys(loadedDelegations).map(key => {
+        loadedDelegationsSerializedKeys[key] = {
+          ...loadedDelegations[key],
+          key_encryption_key: loadedDelegations[key].key_encryption_key.toJSON(),
+          data_encryption_key: loadedDelegations[key].data_encryption_key.toJSON(),
+        };
+      });
+    }
+    const sortedPayload = { ...payloadSortedAlphabetically, loaded_delegations: undefined };
+    return {
+      kind: AuthConfig.kind,
+      metadata: sortedPayload, // Note: SymmetricKey's should stringify with their own `toJSON()`
+      spec: { loaded_delegations: loadedDelegationsSerializedKeys },
+    };
+  }
+
   /**
-   * Create a new AuthData instance from a serialized version
+   * Create a new AuthConfig instance from a serialized version
    */
   static fromJSON(json: any) {
-    return new AuthData({
+    const loadedDelegations = json.loaded_delegations;
+    const loadedDelegationsDeserializedKeys = {};
+    if (loadedDelegations) {
+      Object.keys(loadedDelegations).map(key => {
+        loadedDelegationsDeserializedKeys[key] = {
+          ...loadedDelegations[key],
+          key_encryption_key: SymmetricKey.fromSerialized(
+            loadedDelegations[key].key_encryption_key as string
+          ),
+          data_encryption_key: SymmetricKey.fromSerialized(
+            loadedDelegations[key].data_encryption_key as string
+          ),
+        };
+      });
+    }
+    return new AuthConfig({
       data_encryption_key: SymmetricKey.fromSerialized(json.data_encryption_key),
       key_encryption_key: SymmetricKey.fromSerialized(json.key_encryption_key),
       keystore_access_token: json.keystore_access_token,
@@ -153,13 +177,25 @@ export class AuthConfig {
       vault_access_token: json.vault_access_token,
       delegation_id: json.delegation_id,
       oidc_token: json.oidc_token,
+      loaded_delegations: loadedDelegationsDeserializedKeys,
     });
   }
 
   /**
-   * Allow AuthData to be serialized for easier storage
+   * Allow AuthConfig to be serialized for easier storage
    */
   toJSON() {
+    const loadedDelegations = this.loaded_delegations;
+    const loadedDelegationsSerializedKeys = {};
+    if (loadedDelegations) {
+      Object.keys(loadedDelegations).map(key => {
+        loadedDelegationsSerializedKeys[key] = {
+          ...loadedDelegations[key],
+          key_encryption_key: loadedDelegations[key].key_encryption_key.toJSON(),
+          data_encryption_key: loadedDelegations[key].data_encryption_key.toJSON(),
+        };
+      });
+    }
     return {
       data_encryption_key: this.data_encryption_key.toJSON(),
       key_encryption_key: this.key_encryption_key.toJSON(),
@@ -169,6 +205,7 @@ export class AuthConfig {
       vault_access_token: this.vault_access_token,
       delegation_id: this.delegation_id,
       oidc_token: this.oidc_token,
+      loaded_delegations: loadedDelegationsSerializedKeys,
     };
   }
 }
