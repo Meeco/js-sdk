c0d86113a0b8134fce839d48e5c536b2595c0dc7
Linas Išganaitis <linas.isganaitis@meeco.me>
2022-07-17 13:43:59 +0200

    Fix build and docs commands for some packages

--- packages/file-storage-browser/webpack.config.js
+++ packages/file-storage-browser/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
