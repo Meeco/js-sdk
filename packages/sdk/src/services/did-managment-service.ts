2f87b27817fa17828a464b2eab5018063d10375c
Vijay Shiyani <vshiyani@meeco.me>
2022-09-14 11:27:08 +1000

    added framework to process multi step did create

--- packages/sdk/src/services/did-managment-service.ts
+++ packages/sdk/src/services/did-managment-service.ts
@@ -1,8 +1,10 @@
 import {
+  DIDCreateResultDto,
   DidDocumentDto,
   DIDManagementApi,
   DIDResolutionResultDto,
 } from '@meeco/identity-network-api-sdk';
+import { NewDID } from '../models/new-did';
 import Service, { IIdentityNetworkToken } from './service';
 
 export type MediaType =
@@ -23,7 +25,7 @@ export class DIDManagementService extends Service<DIDManagementApi> {
    * @param credentials
    * @param identifier
    * @param accept - The requested media type of the DID document representation or DID resolution result. See https://www.w3.org/TR/did-core/#representations and https://w3c-ccg.github.io/did-resolution/#did-resolution-result.
-   * @returns Promise<DIDResolutionResultDto | DidDocumentDto>
+   * @returns - Promise<DIDResolutionResultDto | DidDocumentDto> based on accept header input
    */
   public resolve(
     credentials: IIdentityNetworkToken,
@@ -34,5 +36,30 @@ export class DIDManagementService extends Service<DIDManagementApi> {
     return api.didControllerResolve(identifier, accept) as Promise<
       DIDResolutionResultDto | DidDocumentDto
     >;
+  }
+
+  /**
+   * Supported DID key, web, ebsi, sov
+   *
+   * @param credentials
+   * @param newDID - DID to be created e.g. SOV, WEB & KEY
+   * @returns - Promise<DIDCreateResultDto>
+   */
+  public async create(
+    credentials: IIdentityNetworkToken,
+    newDID: NewDID
+  ): Promise<DIDCreateResultDto> {
+    const api = this.identityNetworkAPIFactory(credentials).DIDManagementApi;
+    const initialResult = await api.didControllerCreate(newDID.method, {
+      options: {
+        clientSecretMode: true,
+        network: newDID.network,
+      },
+      didDocument: newDID.didDocument,
+    });
+
+    // process multi-step did creation with client-manage secret
+    const handlerChain = newDID.getHandlerChain();
+    return handlerChain.processRequestResponse(initialResult, api);
   }
 }
