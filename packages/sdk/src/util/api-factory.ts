8fdf3633054ebb12baf4ca5b2b2ce663c656d88f
Linas Išganaitis <linas.isganaitis@meeco.me>
2022-12-22 13:37:01 +0200

    Prefix custom headers with x-; Bump up sdk version to 4.2.9

--- packages/sdk/src/util/api-factory.ts
+++ packages/sdk/src/util/api-factory.ts
@@ -56,9 +56,9 @@ const keystoreAPIKeys =
       // Must be uppercase
       // prettier-ignore
       'Authorization': userAuth.keystore_access_token,
-      'Meeco-Organisation-Id': userAuth.organisation_id || '',
+      'X-Meeco-Organisation-Id': userAuth.organisation_id || '',
       'Meeco-Delegation-Id': userAuth.delegation_id || '',
-      'Meeco-Srt': userAuth.srt || '',
+      'X-Meeco-Srt': userAuth.srt || '',
       authorizationoidc2: userAuth.oidc_token || '',
     }[name] as string);
 
@@ -72,8 +72,8 @@ const vaultAPIKeys = (environment: Environment, userAuth: IVaultToken) => (name:
     // prettier-ignore
     'Authorization': userAuth.vault_access_token,
     'Meeco-Delegation-Id': userAuth.delegation_id || '',
-    'Meeco-Organisation-Id': userAuth.organisation_id || '',
-    'Meeco-Srt': userAuth.srt || '',
+    'X-Meeco-Organisation-Id': userAuth.organisation_id || '',
+    'X-Meeco-Srt': userAuth.srt || '',
     authorizationoidc2: userAuth.oidc_token || '',
   }[name] as string);
 
