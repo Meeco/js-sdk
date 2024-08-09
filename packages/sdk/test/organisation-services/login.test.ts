5d1391a020e69812a5a15f33e305d41beb8d8da2
Fendy Putra <fendy.putra@meeco.me>
2024-08-09 16:38:06 +1000

    fix organisation-services login test

--- packages/sdk/test/organisation-services/login.test.ts
+++ packages/sdk/test/organisation-services/login.test.ts
@@ -1,5 +1,6 @@
 import { OrganizationServicesService } from '@meeco/sdk';
 import { expect } from '@oclif/test';
+import nock from 'nock';
 import { decryptedPrivateKey } from '../fixtures/responses/keypair-response';
 import { customTest, environment, getInputFixture, testUserAuthFixture } from '../test-helpers';
 
@@ -7,28 +8,23 @@ describe('Organization-services login', () => {
   const input = getInputFixture('login-organization-service.input.json');
   const storedToken = 'abc';
 
-  customTest
-    .mockCryppo()
-    .nock('https://sandbox.meeco.me/vault', api =>
-      api
-        .post(`/organizations/${input.organization_id}/services/${input.id}/login`)
-        .matchHeader('Authorization', testUserAuthFixture.vault_access_token)
-        .matchHeader('Meeco-Subscription-Key', 'environment_subscription_key')
-        .reply(200, {
-          token_type: 'bearer',
-          encrypted_access_token: storedToken,
-        })
-    )
-    .it('returns organization service agent login information ', async () => {
-      const orgService = new OrganizationServicesService(environment, testUserAuthFixture);
-      const result = await orgService.getLogin(
-        input.organization_id,
-        input.id,
-        decryptedPrivateKey
-      );
+  before(function () {
+    nock('https://sandbox.meeco.me/vault')
+      .post(`/organizations/${input.organization_id}/services/${input.id}/login`)
+      .matchHeader('Authorization', testUserAuthFixture.vault_access_token)
+      .matchHeader('Meeco-Subscription-Key', 'environment_subscription_key')
+      .reply(200, {
+        token_type: 'bearer',
+        encrypted_access_token: storedToken,
+      });
+  });
 
-      expect(result.vault_access_token).to.match(
-        /^\[decrypted\]abc-----BEGIN RSA PRIVATE KEY-----.*/
-      );
-    });
+  customTest.mockCryppo().it('returns organization service agent login information ', async () => {
+    const orgService = new OrganizationServicesService(environment, testUserAuthFixture);
+    const result = await orgService.getLogin(input.organization_id, input.id, decryptedPrivateKey);
+
+    expect(result.vault_access_token).to.match(
+      /^\[decrypted\]abc-----BEGIN RSA PRIVATE KEY-----.*/
+    );
+  });
 });
